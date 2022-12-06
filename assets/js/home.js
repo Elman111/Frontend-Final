15837c397227e3121510724af053ad1e2082c48c
Elman111 <elmannn@code.edu.az>
2022-12-06 09:24:07 +0400

    Frontend-Final

---
+++ assets/js/home.js
@@ -0,0 +1,7 @@
+let main1=document.getElementById("first-main");
+let main2=document.getElementById("second-main");
+let icn=document.getElementById("simpcar-icon");
+
+icn.addEventListener("click",function(){
+    main2.classList.remove("close")
+})
