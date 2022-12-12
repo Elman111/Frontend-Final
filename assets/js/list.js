913c51d0faeb3d9be4f153d7b4d98f3a7672c9e6
Elman111 <elmannn@code.edu.az>
2022-12-12 11:20:01 +0400

    Search section in home page and list page some codes

--- assets/js/list.js
+++ assets/js/list.js
@@ -2,3 +2,11 @@
 btn.addEventListener("click",function(){
   this.classList.add("close")
 })
+
+let quantityLeft=document.getElementById("left")
+let quantityInp=document.getElementById("quantityInput")
+let quantityRight=document.getElementsByClassName("fa-caret-right")
+
+quantityLeft.addEventListener("click",function(){
+    alert("salam")
+})
